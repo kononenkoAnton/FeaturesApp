636ac702fafab105b42258d4d19a8d6be13c6f89
kononenkoAnton <a.kononenko@applicaster.com>
2024-10-30 12:30:05 -0400

    Initial Commit

---
+++ CountryCodeExampleTests/CountryCodeExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryCodeExampleTests.swift
+//  CountryCodeExampleTests
+//
+//  Created by Anton Kononenko on 10/30/24.
+//
+
+import Testing
+@testable import CountryCodeExample
+
+struct CountryCodeExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
